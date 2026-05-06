1d351fed0d48d6b6ddf44c1a394241383c061139
cjdreamy <josephalocye402@gmail.com>
2026-05-06 06:25:35 +0000

    initializing

---
+++ em.py
@@ -0,0 +1,7 @@
+import subprocess
+import webbrowser
+
+cmd = "echo 'CJ's_AI' | figlet -f slant -c | lolcat"
+cmdM = "sudo apt upgrade && sudo apt update && sudo apt install lolcat figlet"
+url  = f"https://github.com/cjdreamy"
+webbrowser.open(url)
